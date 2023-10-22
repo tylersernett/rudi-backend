bef59649f2ee46fd5ead80311c3e0c4fd8f71e4f
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2023-10-21 23:58:33 -0400

    Add initial DB items

---
+++ tests/test_helper.js
@@ -0,0 +1,24 @@
+
+const initialMetronomes = [
+  {
+    id: 1,
+    title: "Metronome A",
+    bpm: 80,
+    subdivisions: 1,
+    blinkToggle: "off",
+  },
+  {
+    id: 2,
+    title: "Metronome B",
+    bpm: 82,
+    subdivisions: 2,
+    blinkToggle: "downbeat",
+  },
+  {
+    id: 3,
+    title: "Metronome C",
+    bpm: 83,
+    subdivisions: 3,
+    blinkToggle: "all",
+  },
+]
